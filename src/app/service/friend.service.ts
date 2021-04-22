220e50080b6eb16f7a5cbde43adead8021c355c6
trungbe <trungnd0601@gmail.com>
2021-04-22 14:19:23 +0700

    add iuser

--- src/app/service/friend.service.ts
+++ src/app/service/friend.service.ts
@@ -1,11 +1,26 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Router} from '@angular/router';
+import {environment} from '../../environments/environment';
+import {catchError, tap} from 'rxjs/operators';
+import {of} from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class FriendService {
+  private friendUrl = environment.URL + 'relationship';
 
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: HttpClient, private router: Router) {
+  }
+
+  // tslint:disable-next-line:typedef
+  getFriendList(userId: number) {
+    return this.http.get(this.friendUrl + '/listFriend/' + userId).pipe(
+      tap(
+        receivedList => JSON.stringify(receivedList)),
+      catchError(err => of([]))
+    );
+  }
 }
