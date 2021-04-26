e2bfb7fd66d65481bc766640247fbcf12a9dc9d2
mrbeonghia <ntnghia199x@gmail.com>
2021-04-26 12:39:51 +0700

    nothing

--- src/app/post/new-feed/new-feed.component.ts
+++ src/app/post/new-feed/new-feed.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpHeaders} from '@angular/common/http';
-import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-new-feed',
@@ -8,15 +7,10 @@ import {Router} from '@angular/router';
   styleUrls: ['./new-feed.component.css']
 })
 export class NewFeedComponent implements OnInit {
-  currentUser: any;
-  constructor(private router: Router) { }
+
+  constructor() { }
 
   ngOnInit(): void {
-    this.currentUser = localStorage.getItem("currentUser");
-    if (this.currentUser == null) {
-      alert("Bạn phải đăng nhập trước!");
-      this.router.navigate(['/login']);
-    }
   }
 
 }
