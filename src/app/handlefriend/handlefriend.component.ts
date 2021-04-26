c2341764ca2e20e7d4f38f35a4d21a7bd31c00c4
Hung <mqhung93@gmail.com>
2021-04-26 09:44:53 +0700

    done method handle friend request

--- src/app/handlefriend/handlefriend.component.ts
+++ src/app/handlefriend/handlefriend.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FriendService} from '../service/friend.service';
 import {IAppUser} from '../model/IAppUser';
 import {UserService} from '../service/user.service';
@@ -13,25 +13,27 @@ export class HandlefriendComponent implements OnInit {
   userPending: IAppUser;
   pendingList: IAppUser[];
   user: IAppUser;
-  friendList:IAppUser[] ;
+  friendList: IAppUser[];
   sumListFriend: number;
-  userFriend:IAppUser;
+  userFriend: IAppUser;
   userReceiveId: IAppUser;
-  constructor(private friendService: FriendService, private userService: UserService) { }
+
+  constructor(private friendService: FriendService, private userService: UserService) {
+  }
 
   ngOnInit(): void {
-  this.getPendingFriendList();
-  this.getUser();
+    this.getPendingFriendList();
+    this.getUser();
   }
 
   getPendingFriendList() {
     this.userService.getCurrentUser().subscribe(
       response => {
-        this.userPending = <IAppUser>response;
+        this.userPending = <IAppUser> response;
         this.friendService.getPendingFriendList(this.userPending.id).subscribe(
           response => {
-            this.pendingList = <IAppUser[]>response
-              this.sumListPending = this.pendingList.length;
+            this.pendingList = <IAppUser[]> response;
+            this.sumListPending = this.pendingList.length;
           },
           error => console.error(error)
         );
@@ -43,52 +45,27 @@ export class HandlefriendComponent implements OnInit {
   getUser() {
     this.userService.getCurrentUser().subscribe(
       response => {
-        this.user = <IAppUser>response;
+        this.user = <IAppUser> response;
         console.log(this.user);
       },
       error => console.error(error)
-    )
+    );
   }
-  acceptInviteFriend(userSendId:number,statusId:number,index:number){
-    this.userService.findUserById(userSendId).subscribe(
+
+  acceptInviteFriend(userSendId: number, statusId: number, index: number) {
+    this.friendService.handleFriendRequest(userSendId, statusId, {}).subscribe(
       response => {
-        this.user = <IAppUser>response;
-        this.userService.getCurrentUser().subscribe(
-          response => { this.userReceiveId = <IAppUser> response;
-            this.friendService.handleFriendRequest(this.userReceiveId.id,statusId,{
-              "userId": this.user.id,
-              "username": null,
-              "password": null,
-              "birthday": null,
-              "firstName": null,
-              "lastName": null,
-              "gender": null,
-              "phone": null,
-              "email": null,
-              "address": null,
-              "avatar":null,
-              "createdDate":null,
-              "blocked":false,
-              "appRole":null,
-            }).subscribe(
-              response => {
-                if (statusId==3){
-                  this.pendingList.splice(index,1);
-                  this.sumListPending = this.sumListPending-1;
-                } else if (statusId==2){
-                  this.pendingList.splice(index,1);
-                  this.friendList.push(this.user);
-                  this.sumListPending = this.sumListPending-1;
-                  this.sumListFriend=this.sumListFriend+1;
-                }
-              },
-              error => console.error(error)
-            )
-          },
-          error => console.error(error)
-        )
+        if (statusId == 3) {
+          this.pendingList.splice(index, 1);
+          this.sumListPending = this.sumListPending - 1;
+        } else if (statusId == 2) {
+          this.pendingList.splice(index, 1);
+          this.friendList.push(this.user);
+          this.sumListPending = this.sumListPending - 1;
+          this.sumListFriend = this.sumListFriend + 1;
+        }
       },
       error => console.error(error)
-    )
+    );
   }
 }
--- src/app/post/user/user.component.ts
+++ src/app/post/user/user.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {IAppUser} from '../../model/IAppUser';
 import {UserService} from '../../service/user.service';
 import {JwtService} from '../../service/auth/jwt.service';
+import {FriendService} from '../../service/friend.service';
 
 @Component({
   selector: 'app-user',
@@ -12,12 +13,25 @@ export class UserComponent implements OnInit {
   userList: IAppUser[] = [];
   user: IAppUser;
   sumListUser: number;
-  constructor(private userService: UserService,private jwtService: JwtService) {
+  isFriend: boolean = false;
+  str: string = "Add Friend"
+  constructor(private userService: UserService,private jwtService: JwtService, private friendService: FriendService) {
 
   }
 
   ngOnInit(): void {
     this.showAllUser();
+    // this.addFriend();
+    // this.checkFriend();
+  }
+  getUser() {
+    this.userService.getCurrentUser().subscribe(
+      response => {
+        this.user = <IAppUser>response;
+        console.log(this.user);
+      },
+      error => console.error(error)
+    )
   }
 
   showAllUser(){
@@ -32,5 +46,39 @@ export class UserComponent implements OnInit {
           }
         }
       });
+  }
+  // checkFriend(){
+  //   this.userService.getCurrentUser().subscribe(
+  //     response => {this.user = <IAppUser> response;
+  //       var status;
+  //       this.friendService.checkFriend(this.user.id,this.userSendId).subscribe(
+  //         response => {status = response;
+  //           switch (status) {
+  //             case 0:
+  //               this.isFriend = false;
+  //               break;
+  //             case 1:
+  //               this.isFriend = false;
+  //               break;
+  //             case 2:
+  //               this.isFriend = true;
+  //               break;
+  //             case 3:
+  //               this.isFriend = false;
+  //               break;
+  //           }
+  //         },
+  //         error => console.log(error)
+  //       )
+  //     },
+  //     error => console.error(error)
+  //   );
+  // }
+
+  addFriend(userReceiveId: number){
+    this.friendService.sendFriendRequest(userReceiveId, {}).subscribe(() => {
+      this.str = "Send Request";
+      document.getElementById(""+userReceiveId).innerText = this.str;
+    })
   }
 }
